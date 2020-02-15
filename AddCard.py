a9cd5a8aea6a20747e2ef541e68b86ce3eb35e51
tango185 <60274422+tango185@users.noreply.github.com>
2020-02-15 12:05:51 -0700

    Removed level and proc_amt from form and db tables. Realigned contents of form UI. Updated names of remaining tkinter objects.

--- AddCard.py
+++ AddCard.py
@@ -10,28 +10,25 @@ class AddCard:
     def __init__(self, win):
         self.lbl1 = ttk.Label(win, text="Tier: ")
         self.lbl2 = ttk.Label(win, text="Name: ")
-        self.lbl3 = ttk.Label(win, text="Level: ")
-        self.lbl4 = ttk.Label(win, text="M/F: ")
-        self.lbl5 = ttk.Label(win, text="POW: ")
-        self.lbl6 = ttk.Label(win, text="TGH: ")
-        self.lbl7 = ttk.Label(win, text="SPD: ")
-        self.lbl8 = ttk.Label(win, text="CHA: ")
-        self.lbl9 = ttk.Label(win, text="1st Proc: ")
-        self.lbl10 = ttk.Label(win, text="2nd Proc: ")
-        self.lbl11 = ttk.Label(win, text="Proc Amount: ")
-        self.lbl12 = ttk.Label(win, text="U/D/L/R")
+        self.lbl3 = ttk.Label(win, text="M/F: ")
+        self.lbl4 = ttk.Label(win, text="POW: ")
+        self.lbl5 = ttk.Label(win, text="TGH: ")
+        self.lbl6 = ttk.Label(win, text="SPD: ")
+        self.lbl7 = ttk.Label(win, text="CHA: ")
+        self.lbl8 = ttk.Label(win, text="1st Ability: ")
+        self.lbl9 = ttk.Label(win, text="2nd Ability: ")
+        self.lbl10 = ttk.Label(win, text="U/D/L/R")
 
         self.t1 = ttk.Entry()
         self.t2 = ttk.Entry()
-        self.t3 = ttk.Entry()
         v0 = IntVar()
         v0.set(1)
         self.r1 = ttk.Radiobutton(win, text="M", variable=v0, value=1)
         self.r2 = ttk.Radiobutton(win, text="F", variable=v0, value=2)
+        self.t3 = ttk.Entry()
         self.t4 = ttk.Entry()
         self.t5 = ttk.Entry()
         self.t6 = ttk.Entry()
-        self.t7 = ttk.Entry()
         v1 = IntVar()
         v1.set(1)
         self.r3 = ttk.Radiobutton(win, text="POW", variable=v1, value=1)
@@ -44,7 +41,6 @@ class AddCard:
         self.r8 = ttk.Radiobutton(win, text="TGH", variable=v2, value=2)
         self.r9 = ttk.Radiobutton(win, text="SPD", variable=v2, value=3)
         self.r10 = ttk.Radiobutton(win, text="CHA", variable=v2, value=4)
-        self.t8 = ttk.Entry()
         v3 = IntVar()
         v3.set(1)
         self.r11 = ttk.Radiobutton(win, text="U", variable=v3, value=1)
@@ -58,10 +54,10 @@ class AddCard:
         self.lbl2.place(x=50, y=50)
         self.t2.place(x=150, y=50)
         self.lbl3.place(x=50, y=75)
-        self.t3.place(x=150, y=75)
+        self.r1.place(x=150, y=75)
+        self.r2.place(x=200, y=75)
         self.lbl4.place(x=50, y=100)
-        self.r1.place(x=150, y=100)
-        self.r2.place(x=200, y=100)
+        self.t3.place(x=150, y=100)
         self.lbl5.place(x=50, y=125)
         self.t4.place(x=150, y=125)
         self.lbl6.place(x=50, y=150)
@@ -69,25 +65,21 @@ class AddCard:
         self.lbl7.place(x=50, y=175)
         self.t6.place(x=150, y=175)
         self.lbl8.place(x=50, y=200)
-        self.t7.place(x=150, y=200)
+        self.r3.place(x=150, y=200)
+        self.r4.place(x=200, y=200)
+        self.r5.place(x=250, y=200)
+        self.r6.place(x=300, y=200)
         self.lbl9.place(x=50, y=225)
-        self.r3.place(x=150, y=225)
-        self.r4.place(x=200, y=225)
-        self.r5.place(x=250, y=225)
-        self.r6.place(x=300, y=225)
+        self.r7.place(x=150, y=225)
+        self.r8.place(x=200, y=225)
+        self.r9.place(x=250, y=225)
+        self.r10.place(x=300, y=225)
         self.lbl10.place(x=50, y=250)
-        self.r7.place(x=150, y=250)
-        self.r8.place(x=200, y=250)
-        self.r9.place(x=250, y=250)
-        self.r10.place(x=300, y=250)
-        self.lbl11.place(x=50, y=275)
-        self.t8.place(x=150, y=275)
-        self.lbl12.place(x=50, y=300)
-        self.r11.place(x=150, y=300)
-        self.r12.place(x=200, y=300)
-        self.r13.place(x=250, y=300)
-        self.r14.place(x=300, y=300)
-        self.b1.place(x=150, y=325)
+        self.r11.place(x=150, y=250)
+        self.r12.place(x=200, y=250)
+        self.r13.place(x=250, y=250)
+        self.r14.place(x=300, y=250)
+        self.b1.place(x=150, y=275)
 
         print(f"{self.t1.get()}")
 
@@ -104,22 +96,22 @@ class AddCard:
                 gender = 'F'
 
             if v1.get() == 1:
-                proc_1 = 'POW'
+                abil_1 = 'POW'
             elif v1.get() == 2:
-                proc_1 = 'TGH'
+                abil_1 = 'TGH'
             elif v1.get() == 3:
-                proc_1 = 'SPD'
+                abil_1 = 'SPD'
             else:
-                proc_1 = 'CHA'
+                abil_1 = 'CHA'
 
             if v2.get() == 1:
-                proc_2 = 'POW'
+                abil_2 = 'POW'
             elif v2.get() == 2:
-                proc_2 = 'TGH'
+                abil_2 = 'TGH'
             elif v2.get() == 3:
-                proc_2 = 'SPD'
+                abil_2 = 'SPD'
             else:
-                proc_2 = 'CHA'
+                abil_2 = 'CHA'
 
             if v3.get() == 1:
                 arrow = 'U'
@@ -132,15 +124,13 @@ class AddCard:
 
             query = f"INSERT INTO {self.t1.get().lower()}_card VALUES (" \
                     f"\"{self.t2.get()}\", " \
-                    f"{self.t3.get()}, " \
                     f"'{gender}', " \
+                    f"{self.t3.get()}, " \
                     f"{self.t4.get()}, " \
                     f"{self.t5.get()}, " \
                     f"{self.t6.get()}, " \
-                    f"{self.t7.get()}, " \
-                    f"'{proc_1}', " \
-                    f"'{proc_2}', " \
-                    f"{self.t8.get()}, " \
+                    f"'{abil_1}', " \
+                    f"'{abil_2}', " \
                     f"'{arrow}')"
 
             add_card(query)
--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # TBGDBFill
-Program used to manipulate the contents of the MySQL DB "tbgcalc" for use with "TBGCalc" project
+
+## Overview
+TBGDBFill is a standalone program intended for adding cards to the MySQL database "tbgcalc". The purpose of the
+"tbgcalc" database is for use with the TBGCalc program. There are tables setup for the Royal Rumble, Vanguard, Primal,
+and Nightmare tiers.
+
+## Card Input
+
+### POW/TGH/SPD/CHA
+
+All records are explicitly level 100 base stats for Power (POW), Toughness (TGH), Speed (SPD), and Charisma (CHA). Fewer
+than 25 matches can be played on the card for the base stats to remain intact.
+
+### Abilities
+
+While the specific ability stats (POW, TGH, SPD, CHA) need to be selected upon input, the base amount that those stats
+are increased when the ability activates is based on the tier of the card, not its level. Therefore, TBGDBFill will only
+update the "tbgcalc" database with which stats are affected, not the amount. Fewer than 25 matches can be played on the
+card in order to not impact the ability increase.
+
+### Required Inputs
+
+All inputs are required for successful addition into "tbgcalc" database. Required inputs are:
+1. Tier
+2. Name
+3. Gender
+4. POW
+5. TGH
+6. SPD
+7. CHA
+8. First and second Ability stats
+9. Arrow
+
+#### Naming Convention
+
+Normal tier card names should include only the name on the card (i.e. AJ Styles). If the card name includes quotation
+marks, use single quotes (i.e. Bret 'Hit Man' Hart). If the card is an special event card, include the event in
+parentheses following the name (i.e. AJ Styles (Extreme)).
+
+#### Numerical Inputs
+
+Stat amounts must include numerical values only. Do not include commas to separate values in groups of three.
